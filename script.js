501a47e97002039dd5933125d2ae4e860ea4eb14
AmaryllisNO <amaryllisno@gmail.com>
2020-09-22 20:24:50 +0200

    finished task

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ const games = [
     { id: 28, name: "Terrible game", rating: 1.0 },
     { id: 4, name: "Mediocre game", rating: 3.0 },
 ];
+
+const gamesContainer = document.querySelector(".games-list");
+
+games.forEach(game => {
+    let newHTML = "";
+
+    newHTML = gamesContainer.innerHTML += `<li>${game.name}: ${game.rating}</li>`
+
+    console.log(newHTML);
+    listenToListItem();
+})
+
+function listenToListItem() {
+    const games = document.querySelectorAll("li");
+
+    games.forEach(function (game) {
+        game.addEventListener("click", handleClick);
+    })
+
+}
+
+function handleClick() {
+    event.target.classList.toggle("played");
+}
